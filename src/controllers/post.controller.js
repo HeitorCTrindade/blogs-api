e9d3ca78de452cbcbaf843a6155bf5c1444d82b2
HeitorCTrindade <heitor-trindade@hotmail.com>
2022-12-01 20:32:04 -0300

    Requisito 12 - iniciando.

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -1,29 +1,5 @@
-const PostService = require('../services/post.service');
+// const PostService = require('../services/post.service');
 
-const getCategories = async (req, res) => {
-  try {
-    const categories = await CategoryService.getCategories();
-    return res.status(200).json(categories);
-  } catch (err) {
-    return res
-      .status(500)
-      .json({ message: 'Erro interno', error: err.message });
-  }
-};
-
-const creatCategory = async (req, res) => {
-  try {
-    const { name } = req.body;
-    const newCategory = await CategoryService.creatCategory(name);
-    return res.status(201).json(newCategory);
-  } catch (err) {
-    return res
-      .status(500)
-      .json({ message: 'Erro interno', error: err.message });
-  }
-};
-
-module.exports = {
-  getCategories,
-  creatCategory,  
-};
+// module.exports = {
+   
+// };
--- src/services/post.service.js
+++ src/services/post.service.js
@@ -1,28 +1,4 @@
-const { BlogPost } = require('../models');
-const jwtHelpers = require('../auth/jwtHelpers');
+// const { BlogPost } = require('../models');
 
-const getUsers = async () => User.findAll();
-
-const getUserById = async (id) => User.findByPk(id);
-
-const getByUserEmailAndPassword = async (userEmail, userPassword) => 
-  User.findOne({ where: { email: userEmail, password: userPassword } });
-// const getByUserPassword = (userEmail) => User.findOne({ where: { userEmail } });
-
-const getByUserEmail = async (userEmail) => 
-  User.findOne({ where: { email: userEmail } });
-
-const creatUser = async (displayName, email, password, image = null) => {
-  const user = await User.create({ displayName, email, password, image });
-  const { password: _, ...userWithoutPassword } = user.dataValues;
-  const token = jwtHelpers.createToken(userWithoutPassword);
-  return token;
-};
-
-module.exports = {
-  getUsers,
-  getByUserEmailAndPassword,
-  getByUserEmail,
-  creatUser,
-  getUserById,
-};
+// module.exports = {
+// };
